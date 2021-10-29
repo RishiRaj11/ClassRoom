7eb437afa374ce7e597462d7d8e40ab66d59bf3c
Rishi Raj <86799086+RishiRaj11@users.noreply.github.com>
2021-10-29 18:41:49 +0530

    form validation is added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,44 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cpassword, setCpassword] = useState("");
+  const [userType, setUserType] = useState("student");
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const passwordHandler = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const cpasswordHandler = (e) => {
+    setCpassword(e.target.value);
+  };
+
+  const userTypeHandler = (e) => {
+    setUserType(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (email === "") {
+      alert("Email is mandatory");
+    } else if (password === "") {
+      alert("password is mandatory");
+    } else if (password.length < 6) {
+      alert("password length atlest of 6 digits");
+    } else if (password !== cpassword) {
+      alert("password and conform password are not matching");
+    } else if (userType === "") {
+      alert("please select user type");
+    }
+    const userData = {
+      email: email,
+      password: password,
+      cpassword: cpassword,
+      userType: userType,
+    };
+    console.log(userData);
+  };
+
   return (
     <div className="card">
       <h2>Login</h2>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="input_box">
-          <label for="email">
+          <label>
             <b>Email</b>
           </label>
-          <input type="text" placeholder="Enter Email" name="email" required />
+          <input
+            type="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={emailHandler}
+            required
+          />
         </div>
         <div className="input_box">
-          <label for="psw">
+          <label>
             <b>Password</b>
           </label>
           <input
             type="password"
             placeholder="Enter Password"
-            name="psw"
+            value={password}
+            onChange={passwordHandler}
             required
           />
         </div>
         <div className="input_box">
-          <label for="psw">
+          <label>
             <b>Conform Password</b>
           </label>
           <input
             type="password"
             placeholder="Enter Password"
-            name="psw"
+            value={cpassword}
+            onChange={cpasswordHandler}
             required
           />
           <br />
         </div>
         <div className="input_box">
-          <label for="cars">
+          <label>
             <b>Choose user type</b>
           </label>
-          <select name="">
+          <select value={userType} onChange={userTypeHandler}>
             <option value="student">Student</option>
             <option value="teacher">Teacher</option>
           </select>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,58 +1,137 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cpassword, setCpassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [userType, setUserType] = useState("student");
+
+  const nameHandler = (e) => {
+    setName(e.target.value);
+  };
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const passwordHandler = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const cpasswordHandler = (e) => {
+    setCpassword(e.target.value);
+  };
+
+  const phoneHandler = (e) => {
+    setPhone(e.target.value);
+  };
+
+  const userTypeHandler = (e) => {
+    setUserType(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (name === "") {
+      alert("name is mandatory");
+    } else if (email === "") {
+      alert("Email is mandatory");
+    } else if (password === "") {
+      alert("password is mandatory");
+    } else if (password.length < 6) {
+      alert("password length atlest of 6 digits");
+    } else if (password !== cpassword) {
+      alert("password and conform password are not matching");
+    } else if (phone.length !== 10) {
+      alert("phone number must be of 10 digits");
+    } else if (userType==="") {
+      alert("please select user type");
+    }
+    const userData = {
+      name: name,
+      email: email,
+      password: password,
+      cpassword: cpassword,
+      phone: phone,
+      userType: userType,
+    };
+    console.log(userData);
+  };
+
   return (
     <div className="card">
       <h2>SignUp</h2>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="input_box">
-          <label for="text">
+          <label>
             <b>Name</b>
           </label>
-          <input type="text" placeholder="Name" name="name" required />
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={nameHandler}
+            required
+          />
         </div>
         <div className="input_box">
-          <label for="email">
+          <label>
             <b>Email</b>
           </label>
-          <input type="text" placeholder="Enter Email" name="email" required />
+          <input
+            type="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={emailHandler}
+            required
+          />
         </div>
         <div className="input_box">
-          <label for="psw">
+          <label>
             <b>Password</b>
           </label>
           <input
             type="password"
             placeholder="Enter Password"
-            name="psw"
+            value={password}
+            onChange={passwordHandler}
             required
           />
         </div>
         <div className="input_box">
-          <label for="psw">
+          <label>
             <b>Conform Password</b>
           </label>
           <input
             type="password"
             placeholder="Enter Password"
-            name="psw"
+            value={cpassword}
+            onChange={cpasswordHandler}
             required
           />
           <br />
         </div>
         <div className="input_box">
-          <label for="psw">
+          <label>
             <b>Phone number</b>
           </label>
-          <input type="tel" placeholder="Phone number" name="psw" required />
+          <input
+            type="tel"
+            placeholder="Phone number"
+            value={phone}
+            onChange={phoneHandler}
+            required
+          />
           <br />
         </div>
 
         <div className="input_box">
-          <label for="cars">
+          <label>
             <b>Choose user type</b>
           </label>
-          <select name="">
+          <select value={userType} onChange={userTypeHandler}>
+           
             <option value="student">Student</option>
             <option value="teacher">Teacher</option>
           </select>
